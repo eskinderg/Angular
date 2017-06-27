a3dd36338e2b35664636dd7c7b681b0600a0c007
Eskinder <eskinderget@gmail.com>
2017-06-27 15:54:03 -0400

    clean up

--- src/app/theme/perfect-scroll/config.ts
+++
@@ -1,5 +0,0 @@
-import { PerfectScrollbarConfigInterface } from 'angular2-perfect-scrollbar';
-
-export const PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
-  suppressScrollX: true
-};
--- src/app/theme/perfect-scroll/perfect.scrollbar.ts
+++
@@ -1,3 +0,0 @@
-export { PerfectScrollbarModule } from 'angular2-perfect-scrollbar';
-export { PerfectScrollbarConfigInterface } from 'angular2-perfect-scrollbar';
-export { PERFECT_SCROLLBAR_CONFIG } from './config';
