fb3a8518ae1b4094df2dcfb33241fdac171c299f
eskinderg <eskinderget@gmail.com>
2022-03-15 16:10:50 +0300

    rx/js upgrade

--- src/app/effects/events.effect.ts
+++ src/app/effects/events.effect.ts
@@ -1,72 +1,57 @@
 import { Injectable } from '@angular/core';
-import { ofType, Actions, Effect } from '@ngrx/effects';
-import { from, of, Observable, Subject, pipe } from 'rxjs';
+import { ofType, Actions, createEffect } from '@ngrx/effects';
+import { of } from 'rxjs';
 import { catchError, switchMap, map } from 'rxjs/operators';
-import { Store, Action } from '@ngrx/store';
+// import { Store, Action } from '@ngrx/store';
 import * as EventsActions from '../actions/event';
 import { EventDataService } from '../theme/components/event/event.data.service/event.data.service';
 
 @Injectable()
 export class EventsEffect {
 
-  @Effect()
-  save = this.actions$
-    .pipe(ofType(EventsActions.CREATE_EVENT),
-      switchMap((action: EventsActions.CreateEvent) =>
-        this.eventsDataService.createEvent(action.payload)
-        .pipe(
-          map(event => new EventsActions.CreateEventSuccess(event)),
-          catchError(err => of(new EventsActions.CreateEventFail(err)))
-        )
-      )
-    );
+  save = createEffect(()=>
+                      this.actions$.pipe(
+                        ofType(EventsActions.CREATE_EVENT),
+                        switchMap((action: EventsActions.CreateEvent) =>
+                                  this.eventsDataService.createEvent(action.payload)
+                        .pipe(map(event => new EventsActions.CreateEventSuccess(event)),
+                              catchError(err => of(new EventsActions.CreateEventFail(err)))
+                             ))));
 
-  @Effect()
-  update: Observable<Action> = this.actions$
-    .pipe(ofType(EventsActions.UPDATE_EVENT),
-      switchMap((action: EventsActions.UpdateEvent) =>
-        this.eventsDataService.updateEvent(action.payload.newValue)
-        .pipe(
-          map(event => new EventsActions.UpdateEventSuccess(event)),
-          catchError(err => of(new EventsActions.UpdateEventFail(err)))
-        )
-      )
-    );
+  update = createEffect (()=>
+                         this.actions$.pipe(
+                           ofType(EventsActions.UPDATE_EVENT),
+                           switchMap((action: EventsActions.UpdateEvent) =>
+                                     this.eventsDataService.updateEvent(action.payload.newValue)
+                           .pipe(map(event => new EventsActions.UpdateEventSuccess(event)),
+                                 catchError(err => of(new EventsActions.UpdateEventFail(err)))
+                                ))));
 
-  @Effect()
-  toggleEvent: Observable<Action> = this.actions$
-    .pipe(ofType(EventsActions.TOGGLE_EVENT),
-      switchMap((action: EventsActions.ToggleEvent) =>
-        this.eventsDataService.toggleEvent(action.payload)
-        .pipe(
-          map(event => new EventsActions.ToggleEventSuccess(event)),
-          catchError(err => of(new EventsActions.ToggleEventFail(err)))
-        )
-      )
-    );
+  toggleEvent = createEffect(()=>
+                             this.actions$.pipe(
+                               ofType(EventsActions.TOGGLE_EVENT),
+                               switchMap((action: EventsActions.ToggleEvent) =>
+                                         this.eventsDataService.toggleEvent(action.payload)
+                               .pipe( map(event => new EventsActions.ToggleEventSuccess(event)),
+                                     catchError(err => of(new EventsActions.ToggleEventFail(err)))
+                                    ))));
 
-  @Effect()
-  fetch: Observable<Action> = this.actions$
-    .pipe(ofType(EventsActions.FETCH_EVENTS),
-      switchMap(() => this.eventsDataService.getAllEvents()
-        .pipe(
-          map(events => new EventsActions.FetchEventsSuccess(events)),
-          catchError(err => of({ type: EventsActions.FETCH_EVENTS_FAILURE, payload: err }))
-        )
-      )
-    );
+  fetch = createEffect(()=>
+                       this.actions$.pipe(
+                         ofType(EventsActions.FETCH_EVENTS),
+                         switchMap(() => this.eventsDataService.getAllEvents()
+                                   .pipe(map(events => new EventsActions.FetchEventsSuccess(events)),
+                                         catchError(err => of({ type: EventsActions.FETCH_EVENTS_FAILURE, payload: err }))
+                                        ))));
 
-  @Effect()
-  delete = this.actions$
-    .pipe(ofType(EventsActions.DELETE_EVENT),
-      switchMap((action: EventsActions.DeleteEvent) =>
-        this.eventsDataService.deleteEventById(action.payload)
-        .pipe(
-          map(event => new EventsActions.DeleteEventSuccess(event)),
-          catchError(err => of(new EventsActions.DeleteEventFail(err)))
-        )
-      )
-    );
+  delete = createEffect(()=>
+                        this.actions$.pipe(
+                          ofType(EventsActions.DELETE_EVENT),
+                          switchMap((action: EventsActions.DeleteEvent) =>
+                                    this.eventsDataService.deleteEventById(action.payload)
+                          .pipe( map(event => new EventsActions.DeleteEventSuccess(event)),
+                                catchError(err => of(new EventsActions.DeleteEventFail(err)))
+                               ))));
 
   constructor( private actions$: Actions, private eventsDataService: EventDataService) { }
 
--- src/app/reducers/events.ts
+++ src/app/reducers/events.ts
@@ -1,4 +1,4 @@
-import { Action, createFeatureSelector, createSelector } from '@ngrx/store';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
 import * as EventsActions from '../actions/event';
 import { Event } from '../models/event';
 
@@ -16,33 +16,33 @@ export function reducer(state: State = initialState, action: EventsActions.Actio
 
     case EventsActions.EVENTS_CLEAR:
       return {
-        events: []
-      };
+      events: []
+    };
 
     case EventsActions.CREATE_EVENT_SUCCESS:
       return {
-        events: [ action.payload, ...state.events ]
-      };
+      events: [ action.payload, ...state.events ]
+    };
 
     case EventsActions.FETCH_EVENTS_SUCCESS:
       return {
-        events: action.payload || [] // reverse array to show the most recent
-      };
+      events: action.payload || [] // reverse array to show the most recent
+    };
 
     case EventsActions.TOGGLE_EVENT_SUCCESS:
-    case EventsActions.UPDATE_EVENT_SUCCESS:
+      case EventsActions.UPDATE_EVENT_SUCCESS:
       return Object.assign({}, state, {
-        events: state.events.map((event) => {
-          return  (event.id === action.payload.id) ? action.payload : event
-        })
-      });
+      events: state.events.map((event) => {
+        return  (event.id === action.payload.id) ? action.payload : event
+      })
+    });
 
     case EventsActions.DELETE_EVENT_SUCCESS:
       return Object.assign({}, state, {
-        events: state.events.filter((event: Event) => {
-          return event.id !== action.payload.id;
-        })
-      });
+      events: state.events.filter((event: Event) => {
+        return event.id !== action.payload.id;
+      })
+    });
 
     default:
       return state;
