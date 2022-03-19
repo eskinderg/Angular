9014ffa163d8ecffe2c63246ff9d986b4144d6b9
eskinderg <eskinderget@gmail.com>
2022-03-19 13:28:38 +0300

    rxjs

--- src/app/effects/auth.effect.ts
+++ src/app/effects/auth.effect.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core'
-import { Actions, Effect, ofType } from '@ngrx/effects'
-import { empty } from 'rxjs'
+import { Actions, createEffect, ofType } from '@ngrx/effects'
+import { EMPTY } from 'rxjs'
 import { switchMap } from 'rxjs/operators'
 import { Store } from '@ngrx/store'
 import * as AuthActions from '../actions/auth'
@@ -10,69 +10,68 @@ import { Router } from '@angular/router'
 
 @Injectable()
 export class AuthEffect {
-  @Effect({ dispatch: false })
-  login = this.actions$.pipe(ofType(AuthActions.LOGIN_EVENT),
-    switchMap((action: AuthActions.LoginEvent) =>
-      this.oauthService
-        .fetchTokenUsingPasswordFlowAndLoadUserProfile(
+  login = createEffect(() =>
+    this.actions$.pipe(ofType(AuthActions.LOGIN_EVENT),
+      switchMap((action: AuthActions.LoginEvent) =>
+        this.oauthService.fetchTokenUsingPasswordFlowAndLoadUserProfile(
           action.username,
           action.password
         )
-        .then(() => this.store.dispatch(new AuthActions.LoginEventSuccess()))
-        .catch(err => this.store.dispatch(new AuthActions.LoginEventFail(err)))
-    )
-  )
-
-  @Effect({ dispatch: false })
-  loginSuccess = this.actions$.pipe(ofType(AuthActions.LOGIN_EVENT_SUCCESS),
-    switchMap((action: AuthActions.LoginEventSuccess) =>
-      this.oauthService
-        .loadUserProfile()
-        .then(profile => {
-          this.store.dispatch(new AuthActions.LoadProfileSuccess(profile))
-          this.store.dispatch(new EventActions.FetchEvents())
-          this.store.dispatch(new AuthActions.RouteToHome())
-        })
-        .catch(err => this.store.dispatch(new AuthActions.LoadProfileFail(err)))
-    )
-  )
-
-  @Effect({ dispatch: false })
-  tokenExpire = this.actions$.pipe(ofType(AuthActions.TOKEN_EXPIRE),
-    switchMap((action: AuthActions.TokenExpire) => {
-      this.store.dispatch(new AuthActions.Logout(action.message))
-      return empty()
-    })
-  )
-
-  @Effect({ dispatch: false })
-  logout = this.actions$.pipe(ofType(AuthActions.LOGOUT),
-    switchMap((action: AuthActions.Logout) => {
-      this.oauthService.logOut()
-      this.store.dispatch(new EventActions.EventsClear())
-      this.store.dispatch(new AuthActions.RouteToLogin(action.message))
-      return empty()
-    })
-  )
-
-  @Effect({ dispatch: false })
-  routeToHome = this.actions$
-    .pipe(ofType(AuthActions.ROUTE_TO_HOME),
-      switchMap((action: AuthActions.RouteToHome) =>
-        this.router.navigate([`/`])
+          .then(() => this.store.dispatch(new AuthActions.LoginEventSuccess()))
+          .catch(err => this.store.dispatch(new AuthActions.LoginEventFail(err)))
       )
-    )
+    ), { dispatch: false })
 
-  @Effect({ dispatch: false })
-  routeToLogin = this.actions$
-    .pipe(ofType(AuthActions.ROUTE_TO_LOGIN),
-      switchMap((action: AuthActions.RouteToLogin) =>
-        this.router.navigate([
-          `/login`,
-          { endsession: action.message, skipLocationChange: true },
-        ])
+  loginSuccess = createEffect(() =>
+    this.actions$.pipe(ofType(AuthActions.LOGIN_EVENT_SUCCESS),
+      switchMap(() =>
+        this.oauthService
+          .loadUserProfile()
+          .then(profile => {
+            this.store.dispatch(new AuthActions.LoadProfileSuccess(profile))
+            this.store.dispatch(new EventActions.FetchEvents())
+            this.store.dispatch(new AuthActions.RouteToHome())
+          })
+          .catch(err => this.store.dispatch(new AuthActions.LoadProfileFail(err)))
       )
-    )
+    ), { dispatch: false })
+
+  tokenExpire = createEffect(() =>
+    this.actions$.pipe(ofType(AuthActions.TOKEN_EXPIRE),
+      switchMap((action: AuthActions.TokenExpire) => {
+        this.store.dispatch(new AuthActions.Logout(action.message))
+        return EMPTY
+      })
+    ), { dispatch: false })
+
+  logout = createEffect(() =>
+    this.actions$.pipe(ofType(AuthActions.LOGOUT),
+      switchMap((action: AuthActions.Logout) => {
+        this.oauthService.logOut()
+        this.store.dispatch(new EventActions.EventsClear())
+        this.store.dispatch(new AuthActions.RouteToLogin(action.message))
+        return EMPTY
+      })
+    ), { dispatch: false })
+
+  routeToHome = createEffect(() =>
+    this.actions$
+      .pipe(ofType(AuthActions.ROUTE_TO_HOME),
+        switchMap(() =>
+          this.router.navigate([`/`])
+        )
+      ), { dispatch: false })
+
+  routeToLogin = createEffect(() =>
+    this.actions$
+      .pipe(ofType(AuthActions.ROUTE_TO_LOGIN),
+        switchMap((action: AuthActions.RouteToLogin) =>
+          this.router.navigate([
+            `/login`,
+            { endsession: action.message, skipLocationChange: true },
+          ])
+        )
+      ), { dispatch: false })
 
   constructor(
     private oauthService: OAuthService,
