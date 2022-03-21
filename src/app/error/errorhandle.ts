7d9c20986a1c4dae6333b3eae2f9d74e41f1b541
Eskinder <eskinderget@gmail.com>
2022-03-21 16:04:37 +0300

    oauth error handling

--- src/app/error/errorhandle.ts
+++ src/app/error/errorhandle.ts
@@ -1,6 +1,7 @@
 import { ErrorHandler, Injectable, Injector } from '@angular/core';
 import { LocationStrategy, PathLocationStrategy } from '@angular/common';
 import { LoggingService } from './loggingservice';
+import { OAuthService } from 'angular-oauth2-oidc';
 
 // import * as StackTrace from 'stacktrace-js';
 /**
@@ -11,21 +12,26 @@ export class GlobalErrorHandler implements ErrorHandler {
   /**
    * @param {Injector} injector - service for injecting LoggingService
    */
-  constructor(private injector: Injector) { }
+  constructor(private injector: Injector) {
+    this.injector.get(OAuthService).events.subscribe(e => {
+      if (e.type === 'discovery_document_load_error') {
+        this.handleError(e)
+      }
+    })
+  }
 
   /**
    * Handles error
    * @param {error} error - error thrown
    */
-  handleError(error : any) {
+  handleError(error: any) {
 
     const loggingService = this.injector.get(LoggingService);
-    const location       = this.injector.get(LocationStrategy);
-    const message        = error.message ? error.message : error.toString();
-    const url            = location instanceof PathLocationStrategy ? location.path() : '';
+    const location = this.injector.get(LocationStrategy);
+    const message = error.message ? error.message : error.toString();
+    const url = location instanceof PathLocationStrategy ? location.path() : '';
 
     loggingService.error(error);
-
 
     // get the stack trace, lets grab the last 10 stacks only
     // StackTrace.fromError(error).then(stackframes => {
