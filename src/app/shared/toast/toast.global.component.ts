303b99064b56e1bfada46182f7eaf519ccd1e0c5
Eskinder <eskinderget@gmail.com>
2022-03-28 15:58:03 +0300

    change toast position to bottom

--- src/app/shared/toast/toast.global.component.ts
+++ src/app/shared/toast/toast.global.component.ts
@@ -5,7 +5,7 @@ import { ToastService } from './toast.service';
   selector: 'ngbd-toast-global',
   templateUrl: './toast.global.component.html',
   host: {
-    'class': 'toast-container position-fixed top-0 end-0 p-3',
+    'class': 'toast-container position-fixed bottom-0 end-0 p-3',
     'style': 'z-index: 1200'
   }
 })
