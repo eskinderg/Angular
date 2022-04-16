35cbe39740d060be5a5ec643418948e3d9ea78bd
Eskinder <eskinderget@gmail.com>
2022-04-16 16:51:13 +0300

    input timing set

--- src/app/theme/components/textAreaExpanded/textAreaExpanded.component.ts
+++ src/app/theme/components/textAreaExpanded/textAreaExpanded.component.ts
@@ -11,6 +11,7 @@ import {
   ViewChild
 } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { fromEvent, filter, debounceTime, distinctUntilChanged, tap } from 'rxjs';
 
 import { Note } from "../../../models/note";
 
@@ -28,7 +29,7 @@ export const EPANDED_TEXTAREA_VALUE_ACCESSOR: any = {
 })
 export class TextareaExpandedComponent implements ControlValueAccessor, OnInit {
 
-  @ViewChild('textarea', {static:true}) private textarea: ElementRef;
+  @ViewChild('textarea', { static: true }) private textarea: ElementRef;
 
   @Input() textData: string;
   @Input() note: Note;
@@ -36,48 +37,56 @@ export class TextareaExpandedComponent implements ControlValueAccessor, OnInit {
   @Output() onTouched = new EventEmitter(false);
   @Output() onTextChanged = new EventEmitter(false);
 
-  constructor( private renderer: Renderer2 ) { }
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
-    // this.change(this.textData);
-    // this.textData= this.textarea.nativeElement.value;
-    // this.writeValue(this.textData);
+
+    fromEvent(this.textarea.nativeElement, 'input')
+      .pipe(
+        filter(Boolean),
+        debounceTime(450),
+        distinctUntilChanged(),
+        tap(() => {
+          this.onTextChanged.emit({ ...this.note, text: this.textarea.nativeElement.innerHTML });
+        })
+      )
+      .subscribe();
   }
 
-  writeValue( value: string ): void {
+  writeValue(value: string): void {
     const div = this.textarea.nativeElement;
-    this.renderer.setProperty(div, 'innerHTML', value );
+    this.renderer.setProperty(div, 'innerHTML', value);
   }
 
-  registerOnChange( fn: any ): void {
+  registerOnChange(fn: any): void {
     // alert('registerOnChange')
     this.onChange = fn;
   }
 
-  registerOnTouched( fn: any ): void {
+  registerOnTouched(fn: any): void {
     // alert('registerOnTouched')
     this.onTouched = fn;
   }
 
-  setDisabledState( isDisabled: boolean ): void {
+  setDisabledState(isDisabled: boolean): void {
     const div = this.textarea.nativeElement;
     const action = isDisabled ? 'addClass' : 'removeClass';
     this.renderer[action](div, 'disabled');
   }
 
-  change( $event ) {
+  // change($event) {
 
     // console.log(this.textData + $event.data);
     // console.log($event);
     // this.onChange.emit($event);
     // this.onTextChanged.emit($event.target.textContent);
-    console.log($event.target.innerHTML)
+    // console.log($event.target.innerHTML)
 
     // console.log($event.target.innerText);
-    this.onTextChanged.emit({...this.note, text: $event.target.innerHTML});
+    // this.onTextChanged.emit({ ...this.note, text: $event.target.innerHTML });
     // console.log(this.note.text)
 
     // this.onChange($event.target.textContent);
     // this.onTouched($event.target.textContent);
-  }
+  // }
 }
