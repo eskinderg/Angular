4693cf26a95875ebaa65e19141fad55131bbfaa8
Eskinder <eskinderget@gmail.com>
2022-04-18 16:22:48 +0300

    remove router animation

--- src/app/components/shared/animations/fadeInAnimation.ts
+++ src/app/components/shared/animations/fadeInAnimation.ts
@@ -5,10 +5,10 @@ trigger('routerFadeInAnimation', [
   // route 'enter' transition
   // state('*', style({position:'relative'}) ),
 
-  transition(':enter', [
-    // styles at start of transition
-    style({ opacity: 0 }),
-    // animation and styles at end of transition
-    animate('0.25s ease-in-out', style({ opacity: 1 }))
-  ]),
+  // transition(':enter', [
+  //   // styles at start of transition
+  //   style({ opacity: 0 }),
+  //   // animation and styles at end of transition
+  //   animate('0.25s ease-in-out', style({ opacity: 1 }))
+  // ]),
 ]);
