e47374825d18cbc5882be4caf6b711cffa5af577
Eskinder <eskinderget@gmail.com>
2022-05-26 18:24:20 +0300

    remove unwanted interfaces

--- src/app/common/interfaces.ts
+++
@@ -1,8 +0,0 @@
-// import { Event } from '../theme/components/event/event';
-// import { Note  } from '../components/notes/note';
-
-// export interface AppState {
-//   Notes: Note[];
-//   Events: Event[];
-//   VisibilityFilter: any;
-// }
