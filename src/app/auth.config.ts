bb9cf71269199ad6121fdcd6addb7a0c2fc57dd6
Eskinder <eskinderget@gmail.com>
2022-07-18 14:29:34 +0300

    formatting

--- src/app/auth.config.ts
+++ src/app/auth.config.ts
@@ -1,12 +1,12 @@
 import { AuthConfig } from 'angular-oauth2-oidc';
 
 export const authConfig: AuthConfig = {
-  issuer: 'http://192.168.1.5:5000/auth/realms/master',
-  requireHttps: false,
-  redirectUri: window.location.origin,
-  clientId: 'Angular6',
-  responseType: 'code',
-  disableAtHashCheck: true,
-  scope: ' openid profile api2',
-  showDebugInformation: true
+  issuer               : 'http://192.168.1.5:5000/auth/realms/master',
+  requireHttps         : false,
+  redirectUri          : window.location.origin,
+  clientId             : 'Angular6',
+  responseType         : 'code',
+  disableAtHashCheck   : true,
+  scope                : ' openid profile api2',
+  showDebugInformation : true
 }
