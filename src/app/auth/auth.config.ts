dfa68f1529ff5262a312fd86cd7f63a1cb5679f3
Eskinder <eskinderget@gmail.com>
2024-06-14 17:27:20 +0300

    redirect

--- src/app/auth/auth.config.ts
+++ src/app/auth/auth.config.ts
@@ -17,7 +17,7 @@ export const authGoogleConfig: AuthConfig = {
   issuer: 'https://accounts.google.com',
   strictDiscoveryDocumentValidation: false,
   clientId: '35558275617-qa2pthvce7ccunhrv0708lsbnp2vsonj.apps.googleusercontent.com',
-  redirectUri: isDevMode() ? 'http://localhost:4200/Angular' : 'https://eskinderg.github.io/Angular',
+  redirectUri: isDevMode() ? 'http://localhost:4200/Angular' : 'https://eskinderg.github.io',
   scope: 'openid profile email',
   showDebugInformation: true,
   sessionChecksEnabled: true
