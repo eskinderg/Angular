93a3e9aff9ded70dff29e9a6fdad7bb04e72427f
Eskinder <eskinderget@gmail.com>
2024-06-15 17:04:48 +0300

    upgrade to @angular18

--- src/app/auth/auth.JWT.ts
+++ src/app/auth/auth.JWT.ts
@@ -1,18 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class AuthJWT implements HttpInterceptor {
   constructor() {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    request = request.clone({
-      setHeaders: {
-        Authorization: `Bearer ${localStorage.getItem('access_token')}`
-      }
-    });
-
+    // Set authorization header only for local API's
+    if (request.url.includes(environment.API)) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${localStorage.getItem('access_token')}`
+        }
+      });
+    }
     return next.handle(request);
   }
 }
