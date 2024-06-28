b1de5b880eaa6d82128ae82e7102ae3c9afa25b3
Eskinder <eskinderget@gmail.com>
2024-06-28 07:48:28 +0300

    lint

--- src/app/components/movies/movieView/movie-modal/movie-modal.component.spec.ts
+++ src/app/components/movies/movieView/movie-modal/movie-modal.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MovieModalComponent } from './movie-modal.component';
 
 describe('MovieModalComponent', () => {
-  let component: MovieModalComponent;
-  let fixture: ComponentFixture<MovieModalComponent>;
+    let component: MovieModalComponent;
+    let fixture: ComponentFixture<MovieModalComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [MovieModalComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [MovieModalComponent]
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(MovieModalComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(MovieModalComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
