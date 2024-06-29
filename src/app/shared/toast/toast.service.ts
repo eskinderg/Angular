a337ba5a69d3448fafeab0dfedb2be35783c4c3a
Eskinder <eskinderget@gmail.com>
2024-06-29 16:19:06 +0300

    husky config added

--- src/app/shared/toast/toast.service.ts
+++ src/app/shared/toast/toast.service.ts
@@ -12,10 +12,22 @@ export class ToastService {
         this.toasts.splice(0, this.toasts.length);
     }
 
+    /**
+     * Displays success toast message
+     * @param {string} message - text message for the toast to display
+     * @param {string} header - optional param for setting the toast header.
+     * @param {number} delay - optional param for delay setting in milliseconds. Default value is (5000)
+     */
     showSuccess(message: string, header?: string, delay: number = 5000) {
         this.show(message, header, { classname: 'bg-success text-light', delay: delay });
     }
 
+    /**
+     * Displays error toast message
+     * @param {string} message - text message for the toast to display
+     * @param {string} header - optional param for setting the toast header.
+     * @param {number} delay - optional param for delay setting in milliseconds. Default value is (10000)
+     */
     showDanger(message: string, header?: string, delay: number = 10000) {
         this.show(message, header, { classname: 'bg-danger text-light', delay: delay });
     }
