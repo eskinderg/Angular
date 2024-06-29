28c64f5180942a802a640be213f8818720ac2cb4
Eskinder <eskinderget@gmail.com>
2024-06-29 18:07:58 +0300

    clean up

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { fadeInAnimation } from '../shared/animations/fadeInAnimation';
 import { Genre } from './models/genre';
@@ -8,32 +8,15 @@ import { Genre } from './models/genre';
     templateUrl: 'movies.component.html',
     styleUrls: ['movies.component.scss'],
     animations: [fadeInAnimation],
-    // host: { '[@routerFadeInAnimation]': '' }
     changeDetection: ChangeDetectionStrategy.OnPush
+    // host: { '[@routerFadeInAnimation]': '' }
 })
-export class MoviesComponent implements OnInit {
-    public movies: Array<object>;
+export class MoviesComponent {
     public genres: Genre[];
 
     constructor(private route: ActivatedRoute) {
-        // this.moviesApiService.getGenres()
-        // .subscribe(res => {
-        //   this.genres = res;
-        // });
-    }
-
-    search() {
-        // this.moviesApiService.serachMovies(searchStr)
-        //   .subscribe( (res) => {
-        //     this.movies = res.results;
-        //   });
-    }
-
-    ngOnInit() {
         this.genres = this.route.snapshot.data['genres'];
     }
 
-    onSelectGenre() {
-        // alert(genre.name);
-    }
+    onGenreSelect() {}
 }
