6c4580f9ff2bd747e7f9a0b8ce5870acf51e8c71
Eskinder <eskinderget@gmail.com>
2024-10-13 09:22:55 +0300

    organize imports

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,21 +1,21 @@
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideOAuthClient } from 'angular-oauth2-oidc';
+import { provideHttpClient } from '@angular/common/http';
 
+import { AppRouterStateSerializer } from './init/app.route.serilizer';
 import { appRoutes } from './app-routing.module';
 import { APP_INIT } from './app.init';
 import { NgaModule } from './fragments/nga.module';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
-import { AppRouterStateSerializer } from './init/app.route.serilizer';
-import { provideStore } from '@ngrx/store';
 import { appReducer } from './store/reducers';
 import { metaReducers } from './store/reducers';
-import { provideEffects } from '@ngrx/effects';
 import { AuthEffect, EventsEffect, NotesEffect, PreferenceEffect, RouterEffect } from './store/effects';
-import { provideStoreDevtools } from '@ngrx/store-devtools';
-import { provideOAuthClient } from 'angular-oauth2-oidc';
-import { provideHttpClient } from '@angular/common/http';
 import { AuthModule } from './auth/auth.module';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { ThemeModule } from './theme/theme.module';
 
 export const appConfig: ApplicationConfig = {
