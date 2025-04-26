46a43ba5435fe915a2fce4dcb876dd5435053f36
Eskinder <eskinderget@gmail.com>
2025-04-26 20:00:25 +0300

    css adjustment

--- src/app/admin/components/notes/bulk-update-dialog/bulk-update-dialog.component.ts
+++ src/app/admin/components/notes/bulk-update-dialog/bulk-update-dialog.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { DIALOG_RESPONSE, DIALOG_SIGNS, DIALOG_TYPE } from 'src/app/shared/dialog/dialog.enum';
+import { DialogService } from 'src/app/shared/dialog/dialog.service';
 
 @Component({
     selector: 'app-bulk-update-dialog',
@@ -16,7 +18,10 @@ export class BulkUpdateDialogComponent implements OnInit {
 
     form!: FormGroup;
 
-    constructor(private fb: FormBuilder) {}
+    constructor(
+        private fb: FormBuilder,
+        private dialogService: DialogService
+    ) {}
 
     ngOnInit(): void {
         this.form = this.fb.group({
@@ -25,7 +30,6 @@ export class BulkUpdateDialogComponent implements OnInit {
             active: [null]
         });
 
-        // Update userId when owner changes
         this.form.get('owner')?.valueChanges.subscribe((owner) => {
             const user = this.users.find((u) => u[0] === owner);
             this.form.get('userId')?.setValue(user ? user[1] : '', { emitEvent: false });
@@ -37,6 +41,18 @@ export class BulkUpdateDialogComponent implements OnInit {
     }
 
     applyChanges() {
-        this.updated.emit(this.form.value);
+        this.dialogService
+            .openDialog(
+                'Bulk Update',
+                'Do you want to apply bulk update?',
+                DIALOG_TYPE.YES_NO,
+                true,
+                DIALOG_SIGNS.WARNING
+            )
+            .then((result) => {
+                if (result === DIALOG_RESPONSE.YES) {
+                    this.updated.emit(this.form.value);
+                }
+            });
     }
 }
