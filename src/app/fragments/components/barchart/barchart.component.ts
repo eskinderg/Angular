0415d539f4509ead3644c7f287cbbdf742992905
Eskinder <eskinderget@gmail.com>
2025-08-12 16:45:39 +0300

    barchart for admin

--- src/app/fragments/components/barchart/barchart.component.ts
+++ src/app/fragments/components/barchart/barchart.component.ts
@@ -20,7 +20,9 @@ import * as d3 from 'd3';
 export class BarchartComponent implements OnInit {
     chartContainer = viewChild.required<ElementRef>('chart');
 
-    @Input() private data: Array<any> = this.generateSampleData();
+    @Input() public data: Array<any> = this.generateSampleData();
+    @Input() public yHeader: string;
+    @Input() public xHeader: string;
 
     private margin: any = { top: 20, bottom: 20, left: 20, right: 20 };
     private chart: any;
@@ -62,12 +64,11 @@ export class BarchartComponent implements OnInit {
     //     }); // store the initial angles
 
     ngOnInit() {
-        // this.data = this.generateSampleData();
-        // console.log(this.data);
-        const data = this.generateSampleData();
-        this.createChart(data);
-        // console.log(this.data);
-        // this.updateChart();
+        if (!this.data) {
+            this.createChart(this.generateSampleData());
+        } else {
+            this.createChart(this.data);
+        }
     }
 
     // ngOnChanges() {
@@ -80,7 +81,7 @@ export class BarchartComponent implements OnInit {
         const values: Array<any> = [];
 
         for (let i = 0; i < 8 + Math.floor(Math.random() * 10); i++) {
-            values.push({ field: `Index ${i}`, frequency: Math.floor(Math.random() * 100) });
+            values.push({ column: `Column ${i}`, value: Math.round(Math.random() * 100) });
         }
 
         return values;
@@ -98,7 +99,7 @@ export class BarchartComponent implements OnInit {
 
         // const x = d3
         //     .scaleBand()
-        //     .domain(d3.sort(data, (d) => -d.frequency).map((d) => d.field))
+        //     .domain(d3.sort(data, (d) => -d.value).map((d) => d.column))
         //     .range([this.margin.left, this.width - this.margin.right])
         //     .padding(0.1);
 
@@ -107,7 +108,7 @@ export class BarchartComponent implements OnInit {
         // // Create the vertical scale.
         // const y = d3
         //     .scaleLinear()
-        //     .domain([0, d3.max(data, (d) => d.frequency)])
+        //     .domain([0, d3.max(data, (d) => d.value)])
         //     .nice()
         //     .range([this.height - this.margin.bottom, this.margin.top]);
 
@@ -127,9 +128,9 @@ export class BarchartComponent implements OnInit {
         //     .selectAll('rect')
         //     .data(data)
         //     .join('rect')
-        //     .attr('x', (d) => x(d.field))
-        //     .attr('y', (d) => y(d.frequency))
-        //     .attr('height', (d) => y(0) - y(d.frequency))
+        //     .attr('x', (d) => x(d.column))
+        //     .attr('y', (d) => y(d.value))
+        //     .attr('height', (d) => y(0) - y(d.value))
         //     .attr('width', x.bandwidth())
         //     .style('fill', (_d: any, i: any) => this.colors(i));
 
@@ -147,20 +148,14 @@ export class BarchartComponent implements OnInit {
 
         const x = d3
             .scaleBand()
-            .domain(
-                d3.groupSort(
-                    data,
-                    ([d]) => -d.frequency,
-                    (d) => d.field
-                )
-            ) // descending frequency
+            .domain(data.map((d) => d.column))
             .range([this.margin.left, this.width - this.margin.right])
             .padding(0.1);
 
         // Declare the y (vertical position) scale.
         const y = d3
             .scaleLinear()
-            .domain([0, d3.max(data, (d) => d.frequency)])
+            .domain([0, d3.max(data, (d) => d.value)])
             .range([this.height - this.margin.bottom, this.margin.top]);
 
         this.xScale = x;
@@ -180,9 +175,9 @@ export class BarchartComponent implements OnInit {
             .selectAll()
             .data(data)
             .join('rect')
-            .attr('x', (d) => x(d.field))
-            .attr('y', (d) => y(d.frequency))
-            .attr('height', (d) => y(0) - y(d.frequency))
+            .attr('x', (d) => x(d.column))
+            .attr('y', (d) => y(d.value))
+            .attr('height', (d) => y(0) - y(d.value))
             .attr('width', x.bandwidth())
             .style('fill', (_d: any, i: any) => this.colors(i));
 
@@ -196,9 +191,8 @@ export class BarchartComponent implements OnInit {
                     .attr('x', this.width / 2)
                     .attr('y', this.margin.bottom + 20)
                     .attr('fill', '#2a6ada')
-                    .attr('font-weight', 'bold')
                     .attr('font-size', '1.25rem')
-                    .text('← Index →')
+                    .text(this.xHeader ?? '← Index →')
             );
 
         // Add the y-axis and label, and remove the domain line.
@@ -213,17 +207,29 @@ export class BarchartComponent implements OnInit {
                     .attr('y', 0)
                     .attr('fill', '#2a6ada')
                     .attr('font-size', '1.25rem')
-                    .attr('font-weight', 'bold')
                     .attr('text-anchor', 'start')
-                    .text('↑ Frequency (%)')
+                    .text(this.yHeader ?? '↑ value (%)')
             );
+
+        // add label for each bar
+        svg.selectAll('.bar-label')
+            .data(data)
+            .enter()
+            .append('text')
+            .attr('class', 'bar-label')
+            .attr('font-size', '0.75rem')
+            .attr('fill', 'var(--body-color)')
+            .attr('x', (d) => x(d.column) + x.bandwidth() / 2)
+            .attr('y', (d) => y(d.value) - 5) // Adjust offset as needed
+            .attr('text-anchor', 'middle')
+            .text((d) => d.value);
     }
 
     updateChart() {
         // console.log(this.xAxis);
         // update scales & axis
-        this.xScale.domain(this.data.map((d) => d.field));
-        this.yScale.domain([0, d3.max(this.data, (d) => d.frequency)]);
+        this.xScale.domain(this.data.map((d) => d.column));
+        this.yScale.domain([0, d3.max(this.data, (d) => d.value)]);
         this.colors.domain([0, this.data.length]);
         this.xAxis.transition().call(d3.axisBottom(this.xScale));
         this.yAxis.transition().call(d3.axisLeft(this.yScale));
@@ -238,12 +244,12 @@ export class BarchartComponent implements OnInit {
         this.chart
             .selectAll('.bar')
             .transition()
-            .attr('x', (d: any) => this.xScale(d.field))
-            .attr('y', (d: any) => this.yScale(d.frequency))
+            .attr('x', (d: any) => this.xScale(d.column))
+            .attr('y', (d: any) => this.yScale(d.value))
             .attr('width', () => this.xScale.bandwidth())
             .attr('height', (d: any) => {
                 console.log(this.yScale(d[1]));
-                return this.height - this.yScale(d.frequency);
+                return this.height - this.yScale(d.value);
             })
             .style('fill', (_d: any, i: any) => this.colors(i));
 
@@ -252,17 +258,17 @@ export class BarchartComponent implements OnInit {
             .enter()
             .append('rect')
             .attr('class', 'bar')
-            .attr('x', (d: any) => this.xScale(d.field))
-            .attr('y', (d: any) => this.yScale(d.field))
+            .attr('x', (d: any) => this.xScale(d.column))
+            .attr('y', (d: any) => this.yScale(d.column))
             .attr('width', this.xScale.bandwidth())
             .attr('height', 0)
             .style('fill', (_d: any, i: any) => this.colors(i))
             .attr('y', (d: any) => {
-                return this.yScale(d.frequency);
+                return this.yScale(d.value);
             })
             .attr('height', (d: any) => {
-                // console.log(this.height - this.yScale(d.frequency));
-                return this.height - this.yScale(d.frequency);
+                // console.log(this.height - this.yScale(d.value));
+                return this.height - this.yScale(d.value);
             });
     }
 }
