0d0e2749e4e1ad8ccf049ac97c1572bd6f73386a
Eskinder <eskinderget@gmail.com>
2026-03-26 16:32:47 +0300

    angular update

--- src/app/init/app.init.routeStrategy.ts
+++ src/app/init/app.init.routeStrategy.ts
@@ -1,15 +1,22 @@
-import { ActivatedRouteSnapshot, BaseRouteReuseStrategy, DetachedRouteHandle } from '@angular/router';
+import { ActivatedRouteSnapshot, BaseRouteReuseStrategy, DetachedRouteHandle, Route } from '@angular/router';
 
 export class AppRouteReuseStrategy implements BaseRouteReuseStrategy {
+    shouldDestroyInjector(_route: Route): boolean {
+        return true;
+    }
+
     retrieve(_route: ActivatedRouteSnapshot): DetachedRouteHandle | null {
         return null;
     }
+
     shouldAttach(_route: ActivatedRouteSnapshot): boolean {
         return false;
     }
+
     shouldDetach(_route: ActivatedRouteSnapshot): boolean {
         return false;
     }
+
     shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
         if (future.routeConfig === curr.routeConfig) {
             return !future.data['alwaysRefresh'];
@@ -17,5 +24,6 @@ export class AppRouteReuseStrategy implements BaseRouteReuseStrategy {
             return false;
         }
     }
+
     store(_route: ActivatedRouteSnapshot, _handle: DetachedRouteHandle | null): void {}
 }
