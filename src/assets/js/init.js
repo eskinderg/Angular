f9b4f7591865cde2ad8d10bd870bc5efe704fe77
Eskinder <eskinderget@gmail.com>
2026-03-27 17:29:32 +0300

    theme toggle

--- src/assets/js/init.js
+++ src/assets/js/init.js
@@ -1,10 +1,6 @@
 const root = this.document.querySelector(':root');
 
-if (isDarkMode()) {
-    root.classList.add('dark');
-} else {
-    root.classList.remove('dark');
-}
+root.classList.toggle('dark', isDarkMode());
 
 function isDarkMode() {
     return JSON.parse(localStorage.getItem('darkmode')) ?? false;
